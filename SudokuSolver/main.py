9ef73be63b3ccdecba5a710ff2021a64fc12c70b
tomdraper <tomjdraper1@gmail.com>
2020-03-03 16:10:11 +0000

    FIx bug

--- SudokuSolver/main.py
+++ SudokuSolver/main.py
@@ -40,7 +40,7 @@ class Solver:
         # Checks whether the sudoku is complete.
         for i in range(len(sudoku)):
             for j in range(len(sudoku[0])):
-                if apply_constraints(sudoku, i, j) != 0:
+                if self.apply_constraints(sudoku, i, j) != 0:
                     return False
         return True
 
@@ -51,8 +51,8 @@ class Solver:
             for j in range(len(sudoku[0])):
                 if sudoku[i][j] == 0:
                     # If this variable has the fewest number of constraints save as start
-                    if len(apply_constraints(sudoku, i, j)) < min_val:
-                        min_val = len(apply_constraints(sudoku, i, j))
+                    if len(self.apply_constraints(sudoku, i, j)) < min_val:
+                        min_val = len(self.apply_constraints(sudoku, i, j))
                         row, col = i, j  # Save to return
                 if min_val == 1: break
             if min_val == 1: break
@@ -79,18 +79,18 @@ class Solver:
                 
     def depth_first(self, sudoku, row, col):
         # Get constraints for this variable
-        constraints = apply_constraints(sudoku, row, col)
+        constraints = self.apply_constraints(sudoku, row, col)
         
         for val in constraints:
             sudoku[row][col] = val  # Test value
-            new_row, new_col = next_variable(sudoku, row, col)
+            new_row, new_col = self.next_variable(sudoku, row, col)
             
             # If row and column have not changed -> SOLVED
             if new_row == row and new_col == col:
                 return True
             
             # Continue with modifed board on new row and column
-            if depth_first(sudoku, new_row, new_col):
+            if self.depth_first(sudoku, new_row, new_col):
                 return True
         sudoku[row][col] = 0  # Reset this variable on backtrack
         return False
@@ -110,9 +110,14 @@ class Solver:
         
         solved_sudoku = sudoku
         # Choose starting variable with fewest constraints
-        start_row, start_col = lowest_variable(solved_sudoku)  
-        constraints = apply_constraints(solved_sudoku, start_row, start_col)
+        start_row, start_col = self.lowest_variable(solved_sudoku)  
+        constraints = self.apply_constraints(solved_sudoku, start_row, start_col)
+        
+        # Alters the values in the solved_sudoku
         # Begin depth first search from the top left square
-        depth_first(solved_sudoku, 0, 0)
+        self.depth_first(solved_sudoku, 0, 0)
             
         return solved_sudoku
+
+solver = Solver()
+print(solver.sudoku_solver(sudokus[0]))
