22010bfa516726dc2dc5f1babff929c7907958d9
tomdraper <tomjdraper1@gmail.com>
2020-03-03 15:35:09 +0000

    Working

--- SudokuSolver/main.py
+++ SudokuSolver/main.py
@@ -2,20 +2,6 @@ import numpy as np
 
 # Load sudokus
 sudokus = np.load("data/sudokus.npy")
-print("Shape of one sudoku array:", sudokus[0].shape, ". Type of array values:", sudokus.dtype)
-
-# Load solutions
-solutions = np.load("data/sudoku_solutions.npy")
-print("Shape of one sudoku solution array:", solutions[0].shape, ". Type of array values:", solutions.dtype, "\n")
-
-# Print the first sudoku...
-print("Sudoku #1:")
-print(sudokus[0], "\n")
-
-# ...and its solution
-print("Solution of Sudoku #1:")
-print(solutions[0])
-
 
 def apply_constraints(sudoku, row_idx, col_idx):
     possible = [1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -52,33 +38,63 @@ def complete(sudoku):
                 return False
     return True
 
-def nextVariable(sudoku):
+def lowest_variable(sudoku):
+    """Returns the variable found with lowest number of constraints"""
     min_val = len(sudoku)  # Initially hold maximum number of values
-    start_var = tuple()
     for i in range(len(sudoku)):
         for j in range(len(sudoku[0])):
             if sudoku[i][j] == 0:
                 # If this variable has the fewest number of constraints save as start
                 if len(apply_constraints(sudoku, i, j)) < min_val:
                     min_val = len(apply_constraints(sudoku, i, j))
-                    start_var = tuple((i, j))
+                    row, col = i, j  # Save to return
             if min_val == 1: break
         if min_val == 1: break
-    return start_var
+    return row, col
 
-def traverse(sudoku, var, constraints):
+def next_variable(sudoku, row, col):
+    """Returns the next empty variable by searching columns by rows."""
+    
+    new_row, new_col = row, col
+    while True:
+        if new_col + 1 == len(sudoku):
+            new_col = 0
+            if new_row + 1 == len(sudoku):
+                new_row = 0
+            else:
+                new_row += 1
+        else:
+            new_col += 1
+            
+        if sudoku[new_row][new_col] == 0:
+            return new_row, new_col
+        
+        # No available variables
+        if new_row == row and new_col == col:
+            return new_row, new_col
+
+def traverse(sudoku, row, col, constraints):
     if complete(sudoku):
         return
     
-    row, col = var[0], var[1]
     for value in constraints:
         new_sudoku = sudoku[:]
         new_sudoku[row][col] = value
-        new_constraints = apply_constraints(sudoku, row, col)
-        new_var = nextVariable(sudoku)
+        new_row, new_col = next_variable(sudoku, row, col)
+        new_constraints = apply_constraints(sudoku, new_row, new_col)
         
         if len(new_constraints) != 0:
-            traverse(new_sudoku, new_var, new_constraints)
+            traverse(new_sudoku, new_row, new_col, new_constraints)
+            
+def traverse_(sudoku, row, col, complete):
+    constraints = apply_constraints(sudoku, row, col)
+    for val in constraints:
+        sudoku[row][col] = val
+        new_row, new_col = next_variable(sudoku, row, col)
+        if traverse_(sudoku, new_row, new_col, complete):
+            return True
+    sudoku[row][col] = 0  # Reset this variable on backtrack
+    return False
             
 
 def sudoku_solver(sudoku):
@@ -93,16 +109,13 @@ def sudoku_solver(sudoku):
             It contains the solution, if there is one. If there is no solution, all array entries should be -1.
     """
     
-    solved_sudoku = sudoku    
+    print(sudoku)
+    solved_sudoku = sudoku
         
     # Choose starting variable with fewest constraints
-    start_var = nextVariable(solved_sudoku)
-        
-    constraints = apply_constraints(solved_sudoku, start_var[0], start_var[1])
-    print(start_var)
-    print(constraints)
-    
-    solved_sudoku = traverse(sudoku, start_var, constraints)
+    start_row, start_col = lowest_variable(solved_sudoku)  
+    constraints = apply_constraints(solved_sudoku, start_row, start_col)
+    solved_sudoku = traverse_(solved_sudoku, 0, 0, False)
         
     print(solved_sudoku)
     return solved_sudoku
